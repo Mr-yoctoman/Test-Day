1a5e0b9aecf3c512e2d7ea788dd1e0f862828396
Mr.Ashu <145025838+Mr-yoctoman@users.noreply.github.com>
2024-03-09 16:38:22 +0530

    React files
    react

---
+++ react01/script.js
@@ -0,0 +1,7 @@
+let element = React.createElement(
+  "h3",
+  { style: { color: "red" } },
+  "Hello world ! Ashu is Ready with full proof plan!"
+);
+let rootElement = ReactDOM.createRoot(document.getElementById(root));
+rootElement.render(element);
